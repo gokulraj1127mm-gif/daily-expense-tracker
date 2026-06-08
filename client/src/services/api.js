ebd8cab1d44eec7048eecab81faa148ec85dd248
Gokul Raj <gokulraj1127mm@gmail.com>
2026-06-08 17:22:31 +0530

    Connected frontend to Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://daily-expense-tracker-1-itnv.onrender.com/api",
 });
 
 export default API;
